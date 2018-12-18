d9676f22cbafe06147b769b3abf03ab05b1b4b84
Vladimir Moroz <morozvvlad@gmail.com>
2018-12-18 22:51:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/study_book/hw_android_13_moroz_vladimir_s9p1_27/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.study_book.hw_android_13_moroz_vladimir_s9p1_27;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView resultField;
+    EditText numberField;
+    TextView operationField;
+    Double operand = null;
+    String lastOperation = "=";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultField =(TextView) findViewById(R.id.resultField);
+        numberField = (EditText) findViewById(R.id.numberField);
+        operationField = (TextView) findViewById(R.id.operationField);
+    }
+
+
+    public void onNumberClick(View view){
+
+        Button button = (Button)view;
+        numberField.append(button.getText());
+
+        if(lastOperation.equals("=") && operand!=null){
+            operand = null;
+        }
+    }
+
+    public void onOperationClick(View view){
+
+        Button button = (Button)view;
+        String op = button.getText().toString();
+        String number = numberField.getText().toString();
+        if(number.length()>0){
+            number = number.replace(',', '.');
+            try{
+                performOperation(Double.valueOf(number), op);
+            }catch (NumberFormatException ex){
+                numberField.setText("");
+            }
+        }
+        lastOperation = op;
+        operationField.setText(lastOperation);
+    }
+
+    private void performOperation(Double number, String operation){
+
+        if(operand ==null){
+            operand = number;
+        }
+        else{
+            if(lastOperation.equals("=")){
+                lastOperation = operation;
+            }
+            switch(lastOperation){
+                case "=":
+                    operand =number;
+                    break;
+                case "/":
+                    if(number==0){
+                        operand =0.0;
+                    }
+                    else{
+                        operand /=number;
+                    }
+                    break;
+                case "*":
+                    operand *=number;
+                    break;
+                case "+":
+                    operand +=number;
+                    break;
+                case "-":
+                    operand -=number;
+                    break;
+            }
+        }
+        resultField.setText(operand.toString().replace('.', ','));
+        numberField.setText("");
+    }
+
+
+}
